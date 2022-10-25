96bb7276cda61526a9baf92797df6672e6eb9547
Burak Karabekiroglu <burak.karabekiroglu@couchbase.com>
2022-10-25 18:21:19 +0100

    Implement chatUser and chatRoom structs, partial programming of the not yet working Login function.

--- chatServer/Project.go
+++ chatServer/Project.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"net"
 )
 
-type ChatRoom struct {
-	mapOfUsers       map[string]*ChatUser
-	incomingMessages chan string
-	newJoins         chan *ChatUser
-	userDisconnects  chan string
+type ChatRoom struct { // Main room of the server.
+	mapOfUsers       map[string]*ChatUser // Stores all the users and their connection information.
+	incomingMessages chan string          // Receives incoming messages from ChatUser connection.
+	newJoins         chan *ChatUser       // Receives incoming new ChatUser connections.
+	userDisconnects  chan string          // Receives disconnect notifications.
 }
 
-type ChatUser struct{}
-
+// newChatroom is a basic "constructor" for each user.
 func newChatRoom() *ChatRoom {
 	return &ChatRoom{
 		mapOfUsers:       make(map[string]*ChatUser),
@@ -23,9 +23,46 @@ func newChatRoom() *ChatRoom {
 	}
 }
 
-func (chatRoom *ChatRoom) listenForMessages() {}
-func (chatRoom *ChatRoom) Join(net.Conn)      {}
+// ChatUser is a basic "constructor" for each user.
+type ChatUser struct {
+	userConnection   net.Conn // userConnection is the socket.
+	isDisconnected   bool
+	userName         string
+	outgoingMessages chan string   // Channel with all pending outgoing messages.
+	ioReader         *bufio.Reader // Buffered socket read stream.
+	ioWriter         *bufio.Writer // Buffered socket write stream.
+}
 
+func newChatUser(netConnection net.Conn) *ChatUser {
+	writer := bufio.NewWriter(netConnection)
+	reader := bufio.NewReader(netConnection)
+
+	newUser := &ChatUser{
+		userConnection:   netConnection,
+		isDisconnected:   false,
+		ioReader:         reader,
+		ioWriter:         writer,
+		outgoingMessages: make(chan string),
+	}
+
+	return newUser
+}
+
+func (chatRoom *ChatRoom) listenForMessages() {}
+func (chatRoom *ChatRoom) Join(newUserConnection net.Conn) {
+	newUser := newChatUser(newUserConnection)
+
+	if newUser.Login(chatRoom) != nil {
+		chatRoom.newJoins <- newUser
+	}
+}
+
+func (ChatUser *ChatUser) Login(chatRoom *ChatRoom) error {
+
+}
+
+// Function main creates the socket and bind to port 8080 and wait for incoming connections using the loop.
+// Everytime it receives a new connection, it passes it to" chatRoom.Join()".
 func main() {
 	connectionServer, connectionError := net.Listen("tcp", "localhost:8080")
 	if connectionError != nil {
